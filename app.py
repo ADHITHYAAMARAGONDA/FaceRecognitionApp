ecc2341794908b89232749cb09207b26799d3460
Amaragonda Adhithya <adhithyaamaragonda@gmail.com>
2025-03-26 16:34:29 +0530

    Fixed indentation issue

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- from flask import Flask, render_template, request, Response, redirect, url_for
+from flask import Flask, render_template, request, Response, redirect, url_for
 import cv2
 import numpy as np
 import mysql.connector
